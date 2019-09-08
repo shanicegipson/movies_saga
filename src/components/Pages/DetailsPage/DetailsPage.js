6622fadc13bac20d0d4a17707d0388660e0c985a
Shanice Gipson <shanicegipson13@gmail.com>
2019-09-08 16:54:16 -0500

    description and title rendering on description page on click

--- src/components/Pages/DetailsPage/DetailsPage.js
+++ src/components/Pages/DetailsPage/DetailsPage.js
@@ -1,4 +1,4 @@
-import React from 'react'; 
+import React, { Component } from 'react'; 
 import { connect } from 'react-redux';
 import mapStoreToProps from '../../../modules/mapStoreToProps';
 import { withRouter } from 'react-router-dom';
@@ -7,41 +7,40 @@ import BackButton from '../../BackButton/BackButton';
 import EditButton from '../../EditButton/EditButton';
 
 
-function MovieDetails(props) {
-    const movieInfo = props.store.moviesList.filter((movie, index) => {
-    console.log(movieInfo, 'THIS IS A MOVIE!');
-    console.log(props.store.moviesList, 'What do we have?');
-    console.log(movie.movies_id, 'movie ID');
-    console.log(props.match.params.id, 'clicked id');
 
-        return movie.movies_id == props.match.params.id
+class DetailsPage extends Component {
+
+
+    render () {
+        const movieInfo =this.props.store.moviesList.filter((movie, index) => {
+            // console.log(movieInfo, 'THIS IS A MOVIE!');
+            // console.log(this.props.store.moviesList, 'What do we have?');
+            // console.log(movie.movies_id, 'movie ID');
+            // console.log(this.props.match.params.id, 'clicked id'); 
+            console.log(movie, 'name of movie');
         
+                return movie.movies_id == this.props.match.params.id
+        
+            })
 
-    })
-
-    
-    // console.log(movieInfo.title, 'details');
-
-
-    return (
-        <div>
+        return(
+            <div>
             <div>
                 <BackButton />
                 <EditButton />
             </div>
             <div>
-                <h3>Id: {props.match.params.id}</h3>
-                {/* <p>{movieInfo[0].title}</p> */}
-                {/* <p>{movieInfo[0].description}</p>
-                <p>{movieInfo[0].name}</p> */}
+                {/* <h3>Id: {this.props.match.params.id}</h3> */}
+                <h3>{movieInfo[0].title}</h3>
+                <p>{movieInfo[0].description}</p>
+                <p>{movieInfo[0].name}</p>
 
             </div>
         </div>
-    )
-
-
-
-
+        )
+    }
 }
 
-export default connect(mapStoreToProps)(withRouter(MovieDetails));
+
+
+export default connect(mapStoreToProps)(withRouter(DetailsPage));
--- src/components/Pages/EditPage/EditPage.js
+++ src/components/Pages/EditPage/EditPage.js
@@ -3,19 +3,41 @@ import {connect} from 'react-redux';
 import mapStoreToProps from '../../../modules/mapStoreToProps';
 
 class EditPage extends Component {
+    state = {
+        newInfo: {
+            title: '',
+            description: ''
+        }
+    }
+        
 
     cancelEdit = (event) => {
-        this.props.history.push('/details');
+        this.props.history.push('/details/:id');
     }
 
-    editInfo() {
-        console.log('Edit Button');
+    editInfo(event, movieKey) {
+        console.log('event happened')
+        this.setState({
+            newInfo: {
+                ...this.state.newInfo,
+                [movieKey]: event.target.value,
+            }
+        });
     }
+
+
     render() {
         return (
             <div>
-                <button onClick={this.cancelEdit}>Cancel</button>
-                <button onClick={this.editInfo}>Edit</button>
+                <div>
+                    <button onClick={this.cancelEdit}>Cancel</button>
+                    <button onClick={this.editInfo}>Save</button>
+                </div>
+                <div>
+                     <input type='text' value={this.state.newInfo.title} onChange={(event) => this.editInfo(event, 'title')} placeholder="Enter title"/>
+                     <textarea type='text' value={this.state.newInfo.description} onChange={(event) => this.editInfo(event, 'description')} placeholder="Enter description"/>
+                </div>
+                
             </div>
         )
     }
